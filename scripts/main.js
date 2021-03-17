220b24abc458a7f165f70334bd440112b0117c02
fadhilrafiii <fadhilrafi90@gmail.com>
2021-03-17 09:08:42 +0700

    add translation, rotation, and scaling feature

--- scripts/main.js
+++ scripts/main.js
@@ -65,40 +65,29 @@ function main() {
     }
 
     var fieldOfViewRadians = degToRad(70);
-    var fRotationRadians = degToRad(-20);
+    // var fRotationRadians = degToRad(0);
+    var scale = [1, 1, 1];
+    var translation = [0, 0, 0];
+    var rotation = [degToRad(10), degToRad(20), degToRad(40)]
 
     drawScene();
 
-    function updateRotation(event, ui) {
-        fRotationRadians = degToRad(ui.value);
-        drawScene();
-    }
+    // function updateRotation(event, ui) {
+    //     fRotationRadians = degToRad(ui.value);
+    //     drawScene();
+    // }
 
     // Draw the scene.
     function drawScene() {
-        // Tell WebGL how to convert from clip space to pixels
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-        // Clear the canvas AND the depth buffer.
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-        // Turn on culling. By default backfacing triangles
-        // will be culled.
         gl.enable(gl.CULL_FACE);
-
-        // Enable the depth buffer
         gl.enable(gl.DEPTH_TEST);
 
-        // Tell it to use our program (pair of shaders)
         gl.useProgram(program);
-
-        // Turn on the position attribute
         gl.enableVertexAttribArray(positionLocation);
-
-        // Bind the position buffer.
         gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-        // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
         var size = 3; // 3 components per iteration
         var type = gl.FLOAT; // the data is 32bit floats
         var normalize = false; // don't normalize the data
@@ -113,13 +102,9 @@ function main() {
             offset
         );
 
-        // Turn on the normal attribute
         gl.enableVertexAttribArray(normalLocation);
-
-        // Bind the normal buffer.
         gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
 
-        // Tell the attribute how to get data out of normalBuffer (ARRAY_BUFFER)
         var size = 3; // 3 components per iteration
         var type = gl.FLOAT; // the data is 32bit floating point values
         var normalize = false; // normalize the data (convert from 0-255 to 0-1)
@@ -150,16 +135,18 @@ function main() {
         var target = [0, 0, 0];
         var up = [0, 1, 0];
         var cameraMatrix = m4.lookAt(camera, target, up);
-
-        // Make a view matrix from the camera matrix.
+        cameraMatrix = m4.translate(cameraMatrix, translation[0], translation[1], translation[2]);
+        cameraMatrix = m4.scale(cameraMatrix, scale[0], scale[1], scale[2]);
         var viewMatrix = m4.inverse(cameraMatrix);
-
-        // Compute a view projection matrix
         var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
+        
 
-        // Draw a F at the origin
-        var worldMatrix = m4.yRotation(fRotationRadians);
+        // var worldMatrix = m4.xRotation(rotation[0]);
+        var worldMatrix = m4.xRotation(rotation[0]);
+        worldMatrix = m4.yRotate(worldMatrix, rotation[1]);
+        worldMatrix = m4.zRotate(worldMatrix, rotation[2]);
 
+        
         // Multiply the matrices.
         var worldViewProjectionMatrix = m4.multiply(
             viewProjectionMatrix,
@@ -181,7 +168,7 @@ function main() {
         );
 
         // Set the color to use
-        gl.uniform4fv(colorLocation, [0.2, 1, 0.2, 1]); // green
+        gl.uniform4fv(colorLocation, [1, 0, 0, 1]); // green
 
         // set the light direction.
         gl.uniform3fv(
