de265e1493687bffa2bb2f81a005589d36e1b426
fadhilrafiii <fadhilrafi90@gmail.com>
2021-03-21 22:22:42 +0700

    add ui for modify object

--- scripts/event.js
+++ scripts/event.js
@@ -32,8 +32,8 @@ document.querySelectorAll(".slider").forEach(function (el, index) {
             ", #f5f6f8));";
         el.style = style;
         transformArray[index] = parseInt(el.value);
-        document.getElementById(listOfSliderLabel[index]).textContent =
-            el.value;
+        // document.getElementById(listOfSliderLabel[index]).textContent =
+        //     el.value;
     };
     el.oninput();
 });
@@ -120,3 +120,28 @@ const importFile = () => {
         };
     }
 };
+
+let obliqueInput = document.getElementById("oblique");
+let perspectiveInput = document.getElementById("perspective");
+let orthoInput = document.getElementById("orthographic")
+
+const openSlider = e => {
+
+    if (e === "perspective") {
+        perspectiveInput.style.display = "block";
+        obliqueInput.style.display = "none";
+        orthoInput.style.display = "none";
+    } else if (e === "orthogonal") {
+        perspectiveInput.style.display = "none";
+        obliqueInput.style.display = "none";
+        orthoInput.style.display = "block";
+    } else if (e === "oblique") {
+        perspectiveInput.style.display = "none";
+        obliqueInput.style.display = "block";
+        orthoInput.style.display = "block";
+    } else {
+        perspectiveInput.style.display = "none";
+        obliqueInput.style.display = "none";
+        orthoInput.style.display = "none";
+    }
+}
