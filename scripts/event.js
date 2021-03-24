b5492af1b2156d463ab0e061fd23713ad0154760
nisaprmst <13518085@std.stei.itb.ac.id>
2021-03-24 20:31:46 +0700

    add save file

--- scripts/event.js
+++ scripts/event.js
@@ -38,6 +38,21 @@ window.onscroll = function () {
     prevScrollpos = currentScrollPos;
 };
 
+const saveData = (function () {
+    var a = document.createElement("a");
+    // document.body.appendChild(a);
+    // a.style = "display: none";
+    return function (data, fileName) {
+      var json = JSON.stringify(data),
+        blob = new Blob([json], { type: "octet/stream" }),
+        url = window.URL.createObjectURL(blob);
+      a.href = url;
+      a.download = fileName;
+      a.click();
+      window.URL.revokeObjectURL(url);
+    };
+})();
+
 const promptToSave = async () => {
     let fileName = prompt(
         "Do you want to save the data? \n\n If yes give your filename then press 'OK', otherwise press 'Cancel'"
